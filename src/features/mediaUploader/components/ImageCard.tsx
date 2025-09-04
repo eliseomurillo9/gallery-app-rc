a26fa65bdd4d85850ab7499313725ad77634ebae
Eliseo Murillo <eliseomurillo9@gmail.com>
2025-09-04 16:52:46 +0200

    feat: fix css import photo page

--- src/features/mediaUploader/components/ImageCard.tsx
+++ src/features/mediaUploader/components/ImageCard.tsx
@@ -5,13 +5,14 @@ type ImageCardProps = {
   imageDelete?: () => void;
 }
 export function ImageCard({ file, imageDelete }: Readonly<ImageCardProps>) {
+  const fileName = file.name.split('.')[0];
   return (
-    <div className="image-card">
+    <div className="image-card card-animation">
       <div className='image-card__img-wrapper'>
       <img src={URL.createObjectURL(file)} alt={file.name} />
       </div>
       <div className='image-card__img-info'>
-        <h3>{file.name.split('.')[0]}</h3>
+        <h3>{fileName.length > 15 ? fileName.slice(0, 15) + '...' : fileName}</h3>
         <p>{(file.size / 1024).toFixed(2)} KB</p>
         <p>{file.type}</p>
       </div>
--- src/features/mediaUploader/upload.view.tsx
+++ src/features/mediaUploader/upload.view.tsx
@@ -1,40 +1,75 @@
 import { useRef, useState } from "react";
 import { Icon } from "../../shared/UI/Icon/Icon";
-import './upload.css'
+import "./upload.css";
 import { ImageCard } from "./components/ImageCard";
 
 export function UploadView() {
-  const [imagesList, setImagesList] = useState<File[]>([])
-  const inputs = useRef<HTMLInputElement>(null)
+  const [imagesList, setImagesList] = useState<File[]>([]);
+  const inputs = useRef<HTMLInputElement>(null);
   function updatePicList(e: React.ChangeEvent<HTMLInputElement>) {
-    e.preventDefault()
-    const files = inputs.current?.files
+    e.preventDefault();
+    const files = inputs.current?.files;
     if (files) {
-      setImagesList((prev) => [...prev, ...Array.from(files)])
+      setImagesList((prev) => [...prev, ...Array.from(files)]);
     }
   }
 
   function deleteImage(imageIndex: number) {
-    setImagesList(imagesList.filter((_, index) =>  index !== imageIndex))
+    setImagesList(imagesList.filter((_, index) => index !== imageIndex));
   }
 
-    return (
-    <div className="upload-view">
-      {imagesList.length > 0 ? 
-      imagesList.map((file, index) => {
-        return (
-          <ImageCard key={index} file={file} imageDelete={() => deleteImage(index)} />
-        )
-            }) : (
-        <>
-        <pre>{imagesList.length}</pre>
-        <form action="" className="upload-view--form">
-            <input ref={inputs} type="file" multiple accept="image/*" capture="user" onChange={updatePicList}/>
-          </form><div className="upload-button">
-              <Icon name="upload" size="2xl" color="white" />
-            </div><h2>Import your photos</h2><p>Click to choose your photos</p></>
-
+  return (
+    <section className="upload-view">
+      {imagesList.length > 0 ? (
+        <div className="upload-view--image-list">
+          <h1>Upload your photos</h1>
+          <div className="upload-view--preview-image">
+            {imagesList.map((file, index) => {
+              return (
+                <ImageCard
+                  key={index}
+                  file={file}
+                  imageDelete={() => deleteImage(index)}
+                />
+              );
+            })}
+            <div className="upload-view--add-more">
+              <form action="" className="upload-view--form">
+                <input
+                  ref={inputs}
+                  type="file"
+                  multiple
+                  accept="image/*"
+                  capture="user"
+                  onChange={updatePicList}
+                />
+              </form>
+              <div className="upload-button">
+                <Icon name="plus" size="base" color="white" />
+              </div>
+              <h2>Add more photos</h2>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="upload-view--no-images">
+          <form action="" className="upload-view--form">
+            <input
+              ref={inputs}
+              type="file"
+              multiple
+              accept="image/*"
+              capture="user"
+              onChange={updatePicList}
+            />
+          </form>
+          <div className="upload-button">
+            <Icon name="upload" size="2xl" color="white" />
+          </div>
+          <h2>Import your photos</h2>
+          <p>Click to choose your photos</p>
+        </div>
       )}
-    </div>
+    </section>
   );
 }
