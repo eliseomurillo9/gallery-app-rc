705a6b640033968db874c3206e597ae3b02fa9ff
Eliseo Murillo <eliseomurillo9@gmail.com>
2025-10-26 21:44:15 +0100

    feat: control layout show

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 
-import { Outlet } from '@tanstack/react-router'
+import { Outlet, useLocation } from '@tanstack/react-router'
 import './App.css'
 import { Header } from './shared/UI/Header/Header'
 import { NavbarMobile } from './shared/UI/Navbar/mobile/NavbarMobile'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { useEffect, useState } from 'react'
+  
+const PAGES_WITHOUT_MOBILE_LAYOUT = new Set(["/signin"])
 
 function App() {
-
+  const [showLayout, setShowLayout] = useState(true)
+  const location = useLocation().pathname
+  useEffect(() => {
+    setShowLayout(!PAGES_WITHOUT_MOBILE_LAYOUT.has(location))
+  }, [location])
   return (
     <>
-    <Header />
+    {showLayout && <Header />}
      <main>
         <Outlet />
      </main>
-     <NavbarMobile />
+     {showLayout && <NavbarMobile />}
       <TanStackRouterDevtools />
     </>
   )
