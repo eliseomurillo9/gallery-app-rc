de27839b2dd7d01d1636475f16cb05a52d37f645
Eliseo Murillo <eliseomurillo9@gmail.com>
2026-04-15 23:40:23 +0200

    refactor(auth): implement redirection based on user authentication and enhance error handling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,26 @@
-import {createRootRoute } from '@tanstack/react-router'
+import {createRootRoute, redirect} from '@tanstack/react-router'
 import App from '../App'
+import {userStore} from "@/store/user.ts";
 
 export const Route = createRootRoute({
+  beforeLoad: async ({ location }
+  ) => {
+    const user = userStore
+    user.loadUser();
+    const pathRegex = new RegExp(/\/\d+/gm);
+    if (!user.isUserAuthenticated()) {
+      console.error("User NOT authenticated");
+      throw redirect({
+        to: "/signIn",
+        search: {
+          redirect: location.href,
+        },
+      });
+    } else if (!pathRegex.test(location.pathname)) {
+      throw redirect({to: user.getUser().id.toString()})
+    }
+  },
   component: App,
+
 })
 
